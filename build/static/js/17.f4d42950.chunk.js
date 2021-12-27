d4415ea3419dd4a8ca926310ab236bab60cf3e41
Rajaumarali <raja.umerali61@gmail.com>
2021-12-28 01:31:58 +0500

    prod-admin-portal

--- build/static/js/17.f4d42950.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpFleek-Admin-Portal"]=this["webpackJsonpFleek-Admin-Portal"]||[]).push([[17],{1692:function(e,t,a){"use strict";a.r(t);var n=a(0),l=a.n(n);t.default=function(){return l.a.createElement("div",null,l.a.createElement("h2",{className:"title gx-mb-4"},"Dashboard"),l.a.createElement("div",{className:"gx-d-flex justify-content-center"},"      "))}}}]);
-//# sourceMappingURL=17.f4d42950.chunk.js.map
